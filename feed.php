00f6651473be6539c73e03efd2390549ecf6ba71
cuciocj <cuciocj@gmail.com>
2020-03-17 19:26:57 +1300

    implemented dynamic posts

--- feed.php
+++ feed.php
@@ -7,9 +7,26 @@ include_once './dao/postDao.php';
 
 $postDao = new PostDao();
 $posts = $postDao->list(new Post());
-// echo '<pre>';
-// echo var_dump($posts);
-// echo '</pre>'
+
+function getRandomImage() {
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://cataas.com/cat",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return '<img class="card-img-top p-3 p-3" src="data:image/jpeg;base64,'.base64_encode( $response ).'"/>';
+}
 
 ?>
 <!DOCTYPE html>
@@ -40,7 +57,8 @@ $posts = $postDao->list(new Post());
             <div class="col mb-4">
                 <div class="card">
                     <div>
-                        <img src="http://via.placeholder.com/640x360" class="card-img-top p-3 p-3" alt="...">
+                        <!-- <img src="https://cataas.com/cat" class="card-img-top p-3 p-3" alt="..."> -->
+                        <?php echo getRandomImage(); ?>
                     </div>
                     <div class="card-body">
                         <a href="#">
