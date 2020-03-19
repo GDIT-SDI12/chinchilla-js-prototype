564c3ff9822304ed94320b9bd0c23b9141fa98a8
Anton <anton_rozhanskii@yahoo.com>
2020-03-19 15:59:58 +1300

    Edit Pfofile form (no logic)

--- profile.php
+++ profile.php
@@ -1,7 +1,54 @@
 <?php
 include_once './commons/validatesession.php';
+include_once './entity/User.php';
+$user = new User();
+$user = unserialize($_SESSION['user']);
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $user->getUsername();
+    $password = trim($_POST['password']);
+    $oldPassword = trim($_POST['password']);
+    $newPassword = trim($_POST['newPassword']);
+    $newPasswordConfirmation = trim($_POST['newPasswordConfirmation']);
+    $email = trim($_POST['email']);
+    $firstName = trim($_POST['firstName']);
+    $lastName = trim($_POST['lastName']);
+    $phoneNumber = trim($_POST['phoneNumber']);
+
+    $userDao = new UserDao();
+    $user = new User();
+    $errors = array();
+    // Collect data from form
+    if (empty($password)) {
+        $errors[] = "Password is required.";
+    } elseif ($password !== $newPasswordConfirmation) {
+        $errors[] = "Password is not confirmed";
+    } else {
+        $user->setPassword($password);
+    }
+    if (empty($email)) {
+        $errors[] = "Email is required.";
+    } else {
+        $user->setEmail($email);
+    }
+    if (!empty($firstName)) {
+        $user->setFirstName($firstName);
+    }
+    if (!empty($lastName)) {
+        $user->setLastName($lastName);
+    }
+    if (!empty($phoneNumber)) {
+        $user->setPhoneNumber($phoneNumber);
+    }
+
+    if (empty($errors)) {
+        $user->setRole('user');
+        $userDao->create($user);
+        header("location: /login.php");
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +58,7 @@ include_once './commons/validatesession.php';
 <?php require_once 'components/navbar.php' ?>
 <div class="container">
     <div class="row" style="padding-top: 3em;">
-        <div class="col-2">
+        <div class="col-md-2">
             <ul class="nav nav-pills nav-fill flex-column">
                 <li class="nav-item" style="margin-bottom: 1em;">
                     <a class="nav-link btn-outline-primary active" href="#">Edit Profile</a>
@@ -24,61 +71,80 @@ include_once './commons/validatesession.php';
                 </li>
             </ul>
         </div>
-        <div class="col-10">
+        <div class="col-md-10">
             <div class="tab-content" id="v-pills-tabContent">
                 <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel"
                      aria-labelledby="v-pills-home-tab">
                     <form method="" action="" style="padding : 0 15% 15%;">
                         <div class="form-group row">
-                            <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
-                            <div class="col-sm-10">
-                                <input type="username" class="form-control" id="inputUsername"
-                                       placeholder="username123" disabled>
+                            <label for="inputUsername" class="col-sm-3 col-form-label">Username</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="username" name="username"
+                                       placeholder="<?= $user->getUsername(); ?>" disabled>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputFirstname" class="col-sm-2 col-form-label">First name</label>
-                            <div class="col-sm-10">
-                                <input type="firstname" class="form-control" id="inputFirstname"
-                                       placeholder="Richard">
+                            <label for="firstname" class="col-sm-3 col-form-label">First name</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="firstName" name="firstName"
+                                       placeholder="<?= $user->getFirstName(); ?>">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputLastname" class="col-sm-2 col-form-label">Last name</label>
-                            <div class="col-sm-10">
-                                <input type="lastname" class="form-control" id="inputLastname"
-                                       placeholder="Hendricks">
+                            <label for="lastname" class="col-sm-3 col-form-label">Last name</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="lastName" name="lastName"
+                                       placeholder="<?= $user->getLastName(); ?>">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputPhonenumber" class="col-sm-2 col-form-label">Phone number</label>
-                            <div class="col-sm-10">
-                                <input type="phonenumber" class="form-control" id="inputPhonenumber"
-                                       placeholder="0221234567">
+                            <label for="phoneNumber" class="col-sm-3 col-form-label">Phone number</label>
+                            <div class="col-sm-9">
+                                <input type="text" class="form-control" id="phoneNumber" name="phoneNumber"
+                                       placeholder="<?= $user->getPhoneNumber(); ?>">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-                            <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputEmail"
-                                       placeholder="richardhendricks@piedpiper.com">
+                            <label for="email" class="col-sm-3 col-form-label">Email</label>
+                            <div class="col-sm-9">
+                                <input type="email" class="form-control" id="email" name="email"
+                                       placeholder="<?= $user->getEmail(); ?>">
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                            <div class="input-group col-sm-10">
-                                <input type="password" class="form-control" id="inputPassword" placeholder="******"
-                                       aria-describedby="button-addon2">
-                                <div class="input-group-append">
-                                    <button class="btn btn-outline-secondary" type="button"
-                                            id="button-addon2">Change Password
-                                    </button>
+                        <div class="row">
+                            <p>
+                                <a class="btn btn-primary float-right" data-toggle="collapse"
+                                   href="#changePasswordDialog">Change Password</a>
+                            </p>
+                        </div>
+                        <div class="row">
+                            <div class="col collapse" id="changePasswordDialog">
+                                <div class="form-group row">
+                                    <label for="oldPassword" class="col-sm-3 col-form-label">Old Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="password" class="form-control" id="oldPassword" name="oldPassword">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="newPassword" class="col-sm-3 col-form-label">New Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="password" class="form-control" id="newPassword" name="newPassword">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="newPasswordConfirmation" class="col-sm-3 col-form-label">Confirm New
+                                        Password</label>
+                                    <div class="col-sm-9">
+                                        <input type="password" class="form-control" id="newPasswordConfirmation"
+                                               name="newPasswordConfirmation">
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <a href="profile.php" style="text-decoration: none;">
-                            <button type="button" class="btn btn-outline-primary col-md-3 offset-md-9">Save</button>
-                        </a>
+                        <div>
+                            <button type="submit" class="btn btn-default btn-outline-primary inline float-right">Save
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
