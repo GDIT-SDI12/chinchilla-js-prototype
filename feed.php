ee064a488f645d9bf57190a9895c00a5fe415c3c
CJ Cucio <cuciocj@gmail.com>
2020-03-26 18:35:46 +1300

    :hammer: removed unused function

--- feed.php
+++ feed.php
@@ -7,26 +7,6 @@ include_once './dao/postDao.php';
 $postDao = new PostDao();
 $posts = $postDao->list(new Post());
 
-function getRandomImage() {
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://cataas.com/cat",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-    ));
-
-    $response = curl_exec($curl);
-
-    curl_close($curl);
-    return '<img class="card-img-top p-3 p-3" src="data:image/jpeg;base64,'. base64_encode( $response ) .'"/>';
-}
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
