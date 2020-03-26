24e64338bf36a9bd2ab96b8e4b2d0426200b1d92
Anton <anton_rozhanskii@yahoo.com>
2020-03-26 13:24:34 +1300

    Fix "Something went wrong"

--- post.php
+++ post.php
@@ -17,7 +17,12 @@ if (isset($_GET['post'])) {
     exit;
 }
 $postDao = new PostDao();
+$userDao = new UserDao();
 $post = $postDao->find($postId);
+$author = $userDao->find($post->getAuthor());
+
+$phoneNumber = $author->getPhoneNumber();
+$email = $author->getEmail();
 
 if ($post == null) {
     header('location: /unavailablePost.php');
@@ -29,21 +34,41 @@ if ($post == null) {
 <body>
 <?php require_once 'components/navbar.php' ?>
 <div class="container">
-    <div class="row">
-        <div class="col">
+    <div class="row justify-content-between">
+        <div class="col-md-4 my-5">
             <div>
                 <?php if (null !== $post->getImages()) { ?>
-                    <img src="<?= DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR . str_replace("#DS#", DIRECTORY_SEPARATOR, array_values($post->getImages())[0]) ?>" class="card-img-top p-3 p-3">
+                    <img src="<?= DIRECTORY_SEPARATOR . "uploads" . DIRECTORY_SEPARATOR . str_replace("#DS#", DIRECTORY_SEPARATOR, array_values($post->getImages())[0]) ?>"
+                         class="card-img-top p-3 p-3">
                 <?php } else { ?>
                     <img src="http://via.placeholder.com/640x360" class="card-img-top p-3 p-3" alt="...">
                 <?php } ?>
             </div>
+            <form class="mt-5">
+                <div class="form-group row mb-0">
+                    <label for="email" class="col-sm-3 col-form-label-sm">Phone:</label>
+                    <div class="col-sm-9">
+                        <label class="form-control-sm"
+                               id="email"><?= ($phoneNumber) ? $phoneNumber : "hidden" ?></label>
+                    </div>
+                </div>
+                <div class="form-group row mb-0">
+                    <label for="email" class="col-sm-3 col-form-label-sm">Email:</label>
+                    <div class="col-sm-9">
+                        <label class="form-control-sm" id="email"><?= $email ?></label>
+                    </div>
+                </div>
+                <button class="btn btn-outline-primary btn-block">Save</button>
+            </form>
         </div>
-        <div class="col">
-            <?php
-                echo "<pre>";
-                var_dump($post);
-            ?>
+        <div class="col-md-7 ml-3">
+            <div class="row my-5">
+                <h1><?= $post->getTitle(); ?></h1>
+            </div>
+
+            <div class="row mb-3">
+                <?= $post->getBody(); ?>
+            </div>
         </div>
     </div>
 </div>
