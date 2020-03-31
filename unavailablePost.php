3f6fe7be32307f9146ffb3f8bc864954e39a31cc
CJ Cucio <cuciocj@gmail.com>
2020-03-31 15:46:56 +1300

    :new: implemented 404 page if post is not found

---
+++ unavailablePost.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'commons/validatesession.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once 'components/head.php' ?>
+
+<body>
+<?php require_once 'components/navbar.php' ?>
+<div class="container">
+    <div class="logo" style="text-align: center;">
+        <img src="images/android-chrome-384x384.png" alt="logo"/>
+    </div>
+    <div style="text-align: center; padding : 0 35% 35%;">
+        <h2>404 Page NOT Found</h2>
+        <a href="index.php" style="text-decoration: none;">
+            <button type="button" class="btn btn-primary btn-block">Go Back!</button>
+        </a>
+    </div>
+</div>
+</body>
+
+</html>
